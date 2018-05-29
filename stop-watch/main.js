fdba0d5a27dbc09e02493bfb167f79598c9767b4
taylorjosephgriffin <taylorjosephgriffin@yahoo.com>
2018-05-29 15:59:48 -0700

    Revised EventListener to clear interval when start button is clicked and watch is running. #2

--- stop-watch/main.js
+++ stop-watch/main.js
@@ -3,12 +3,19 @@
 let $button = document.querySelector('#start')
 let $time = document.querySelector('#time')
 let isRunning = false
+let interval = null
 
 function startTime() {
-  isRunning = true
   $time.innerHTML++
 }
 
 $button.addEventListener('click', function () {
-  setInterval(startTime, 1000)
+  if (!isRunning) {
+    isRunning = true
+    interval = setInterval(startTime, 1000)
+  }
+  else if (isRunning) {
+    isRunning = false
+    clearInterval(interval)
+  }
 }, false)
