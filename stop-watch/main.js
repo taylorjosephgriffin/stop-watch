ef431d7f4d7cafa657522684132f264b61960fb5
taylorjosephgriffin <taylorjosephgriffin@yahoo.com>
2018-05-29 16:22:13 -0700

    Changed EventListener to change textContent of  to indicate watch state. #2

--- stop-watch/main.js
+++ stop-watch/main.js
@@ -11,9 +11,14 @@ $button.addEventListener('click', function () {
   if (!isRunning) {
     isRunning = true
     interval = setInterval(startTime, 1000)
+    setTimeout(function () {
+      $button.textContent = 'pause'
+    }, 1000)
+
   }
   else if (isRunning) {
     isRunning = false
     clearInterval(interval)
+    $button.textContent = 'start'
   }
 }, false)
