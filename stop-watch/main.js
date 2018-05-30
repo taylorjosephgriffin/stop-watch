dd559f0ffc6ebdb325c10ea40426182cf37f740d
taylorjosephgriffin <taylorjosephgriffin@yahoo.com>
2018-05-30 13:39:24 -0700

    Updated tick function to conditionally stop timer. #4

--- stop-watch/main.js
+++ stop-watch/main.js
@@ -1,11 +1,23 @@
 const $start = document.querySelector('#start')
 const $time = document.querySelector('#time')
 const $reset = document.querySelector('#reset')
+const $form = document.querySelector('#timelimit')
+const $confirm = document.querySelector('#confirm')
+let timeLimit = null
+let limitSet = false
 let isRunning = false
 let intervalStart = null
 
 function startTime() {
-  $time.textContent++
+  if ($time.textContent < timeLimit && timeLimit !== null) {
+    $time.textContent++
+  }
+  else if (timeLimit === null || timeLimit === 0 || timeLimit === '') {
+    $time.textContent++
+  }
+  else {
+    clearInterval(intervalStart)
+  }
 }
 
 function resetTime() {
@@ -23,7 +35,6 @@ $start.addEventListener('click', function () {
       $start.textContent = 'pause'
       $start.style.backgroundColor = '#FF9F9F'
     }, 1000)
-
   }
   else if (isRunning) {
     isRunning = false
@@ -40,4 +51,18 @@ $reset.addEventListener('click', function () {
   $reset.hidden = true
   $start.textContent = 'start'
   $start.style.backgroundColor = 'transparent'
+  if (limitSet) {
+    timeLimit = 0
+    $confirm.style.backgroundColor = 'transparent'
+    $confirm.textContent = 'SET'
+  }
+}, false)
+
+$confirm.addEventListener('click', function () {
+  timeLimit = $form.value
+  limitSet = true
+  if (timeLimit !== '') {
+    $confirm.style.backgroundColor = 'rgba(120, 222, 196, 1)'
+    $confirm.textContent = 'time limit is set'
+  }
 }, false)
